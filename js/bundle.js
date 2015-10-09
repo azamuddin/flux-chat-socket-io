31a807479042be43eae8a8cf5d2c1ceb20ed629e
Muhammad Azamuddin <azamuddin@live.com>
2015-10-09 23:29:28 +0700

    Update bundle.js

--- js/bundle.js
+++ js/bundle.js
@@ -346,6 +346,9 @@ var ChatStore = assign({}, EventEmitter.prototype, {
   },
   setCurrentUser: function(user){
     _currentUser = user;
+  },
+  setAllUsers: function(users){
+    _allUsers = users;
   }
 });
 
@@ -36166,9 +36169,7 @@ function drainQueue() {
         currentQueue = queue;
         queue = [];
         while (++queueIndex < len) {
-            if (currentQueue) {
-                currentQueue[queueIndex].run();
-            }
+            currentQueue[queueIndex].run();
         }
         queueIndex = -1;
         len = queue.length;
@@ -36220,6 +36221,7 @@ process.binding = function (name) {
     throw new Error('process.binding is not supported');
 };
 
+// TODO(shtylman)
 process.cwd = function () { return '/' };
 process.chdir = function (dir) {
     throw new Error('process.chdir is not supported');
